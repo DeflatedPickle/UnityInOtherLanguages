fd66743d9e2b01e3377cf438722bd80c3bbed563
DeflatedPickle <DeflatedPickle@gmail.com>
2018-08-19 04:22:20 +0100

    Added a Haxe example

---
+++ UnityInOtherLanguages/Assets/Scripts/Haxe/HaxeScript.hx
@@ -0,0 +1,9 @@
+﻿import unityengine.MonoBehaviour;
+import unityengine.Debug;
+
+@:nativeGen class HaxeScript extends MonoBehaviour {
+	public static function main() { }
+	public function Start (): Void {
+		Debug.Log("Starting from Haxe!");
+	}
+}
